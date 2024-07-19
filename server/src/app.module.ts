6bf1936cfdb4497b44702d18502ab42136ae7110
nguyen.nk <44081478+khanhnguyendev@users.noreply.github.com>
2024-07-19 15:24:40 +0700

    #nestjs WebSockets + Order Services initial

---
+++ server/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrderModule } from './order/order.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Module({
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), OrderModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
